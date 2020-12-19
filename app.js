f285470e1365178c4bd3ba101bf267f9570c4792
Brandt <frickerbrandt@gmail.com>
2020-12-19 15:47:01 -0700

    inquirer prompts started

--- app.js
+++ app.js
@@ -9,23 +9,143 @@ var connection = mysql.createConnection({
     database: "employees_db"
   });
 
-function getAllRowsFromTable(table,cb){
-    const queryString = `SELECT * FROM ${table}`;
-    connection.query(queryString, function(err,rows){
-        if (err){
+// function getAllRowsFromTable(table,cb){
+//     const queryString = `SELECT * FROM ${table}`;
+//     connection.query(queryString, function(err,rows){
+//         if (err){
            
-            cb(err);
-        }else{
-            cb(null,rows);
+//             cb(err);
+//         }else{
+//             cb(null,rows);
 
+//         }
+//     })
+// }
+// getAllRowsFromTable('departments',function(err,rows){
+//     if(err){
+//         console.log("Error getting all rows: ",err);
+//     }else{
+//         console.table(rows);
+//     }
+// });
+
+connection.connect(function(err) {
+    if (err){
+        console.log("YOU HAVE AN ERROR: "+err);
+    };
+    start();
+    getDepartments();
+    getRoles();
+    getManagers();
+    getEmployees();
+  });
+
+  function start(){ 
+
+    inquirer
+      .prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"]
+      })
+      .then(function(answer) {
+           if (answer.choices === "VIEW") {
+            view();
+          } 
+        else if (answer.choices === "ADD") {
+          add();
         }
+        else if (answer.choices === "UPDATE") {
+          update();
+        }
+        else if (answer.choices === "DELETE") {
+          deleteInput();
+        }
+        else if (answer.choices === "EXIT") {
+         
+        
+          connection.end();
+        }
+        else{
+          connection.end();
+        }
+      });
+  }
+  function getRoles(){
+    connection.query("SELECT id, title FROM roles", (err, res) => {
+      if (err) throw err;
+      roles = res;
+    
     })
-}
-getAllRowsFromTable('department',function(err,rows){
-    if(err){
-        console.log("Error getting all rows: ",err);
-    }else{
-        console.table(rows);
-    }
-});
-
+  };
+  
+  function getDepartments() {
+    connection.query("SELECT id, name FROM departments", (err, res) => {
+      if (err) throw err;
+      departments = res;
+     
+    })
+  };
+  
+  function getManagers(){
+    connection.query("SELECT id, first_name, last_name FROM employee", (err, res) => {
+      if (err) throw err;
+      managers = res;
+      
+    })
+  };
+  
+  function getEmployees(){
+    connection.query("SELECT id FROM employee", (err, res) => {
+      if (err) throw err;
+      employees = res;
+      
+    })
+  };
+  function add() {
+    inquirer.prompt([
+      {
+        name: "add",
+        type: "list",
+        message: "What would you like to add?",
+        choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "EXIT"]
+      }
+    ]).then(function(answer) {
+      if (answer.add === "DEPARTMENT") {
+        console.log("Add a new: " + answer.add);
+        addDepartment();
+      }
+      else if (answer.add === "ROLE") {
+        console.log("Add a new: " + answer.add);
+        addRole();
+      }
+      else if (answer.add === "EMPLOYEE") {
+        console.log("Add a new: " + answer.add);
+        addEmployee();
+      } 
+      else if (answer.add === "EXIT") {
+        
+  
+        connection.end();
+      } else {
+        connection.end();
+      }
+    })
+  };
+  function addDepartment() {
+    inquirer.prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "What department would you like to add?"
+      }
+    ]).then(function(answer) {
+      connection.query(`INSERT INTO department (name) VALUES ('${answer.department}')`, (err, res) => {
+        if (err) throw err;
+        console.log("New department added: " + answer.department);
+        getDepartments();
+        start();
+      }) 
+    })
+  };
--- schema.sql
+++ schema.sql
@@ -3,19 +3,19 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (30),
     PRIMARY KEY (id) 
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR (30),
     salary DECIMAL (10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)   
+    FOREIGN KEY (department_id) REFERENCES departments(id)   
 );
 
 CREATE TABLE employee (
@@ -25,14 +25,14 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)  
 );
 
-INSERT INTO department(name)
+INSERT INTO departments(name)
 Values ('Sales'), ('Marketing'),('Engineering'),('Human Resources');
 
-INSERT INTO role(title, salary, department_id) Values ('Sales Associate',40000,1),('Research and Development',50000,2),('Engineer',60000,3),('HR',30000,4),('Sales Manager',75000,1),('Marketing Manager',75000,2),('Engineering Manager',75000,3),('Human Resources Manager',75000,4);
+INSERT INTO roles(title, salary, department_id) Values ('Sales Associate',40000,1),('Research and Development',50000,2),('Engineer',60000,3),('HR',30000,4),('Sales Manager',75000,1),('Marketing Manager',75000,2),('Engineering Manager',75000,3),('Human Resources Manager',75000,4);
 
 INSERT INTO employee(first_name,last_name,role_id,manager_id) Values ('Spider','Man',5,NULL),('Iron','Man',1,1),('Black','Panther',6,NULL),('Dead','Pool',2,3),('Captain','America',7,NULL),('Jessica','Jones',3,5),('Ant','Man',8,NULL),('Captain','Marvel',4,7);
 
